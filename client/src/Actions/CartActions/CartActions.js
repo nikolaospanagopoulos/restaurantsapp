860939cafc0ff7333fbd9c2439286a177e4bfc8e
nikospan <nikos4222@outlook.com.gr>
2021-03-07 01:02:06 +0200

    add to cart functionality

--- client/src/Actions/CartActions/CartActions.js
+++ client/src/Actions/CartActions/CartActions.js
@@ -8,11 +8,11 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
-            product: data._id,
-            name: data.name,
-            image: data.image,
-            price: data.price,
-            available: data.available,
+            product: data.data._id,
+            name: data.data.name,
+            image: data.data.image,
+            price: data.data.price,
+            available: data.data.available,
             qty
 
         }
--- client/src/Pages/CartPage/CartPage.jsx
+++ client/src/Pages/CartPage/CartPage.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../../Components/Message/Message";
+import { Link } from "react-router-dom";
+import { addToCart } from "../../Actions/CartActions/CartActions";
 
+const CartPage = ({ match, location, history }) => {
+  const productId = match.params.id;
 
-const CartPage = () => {
-    return ( 
-        <div>
-            
-        </div>
-     );
-}
- 
+  //get qty from url
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+  return <div></div>;
+};
+
 export default CartPage;
