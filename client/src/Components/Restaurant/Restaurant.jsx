fae97373d9168f8da74b7f167931678eac7a6a73
nikospan <nikos4222@outlook.com.gr>
2021-03-06 16:23:00 +0200

    added regex to remove useless address words

--- client/src/Components/Restaurant/Restaurant.jsx
+++ client/src/Components/Restaurant/Restaurant.jsx
@@ -1,14 +1,29 @@
 import React from "react";
-import './Restaurant.css'
-import {Link} from 'react-router-dom'
+import "./Restaurant.css";
+import { Link } from "react-router-dom";
 
-const Restaurant = ({ restaurant}) => {
+const removeWord = (string) => {
+  {
+    const uselessWordsArray = ["Municipal", "Municipality", "of", "Unit"];
+
+    const expStr = uselessWordsArray.join("|");
+    return string
+      .replace(new RegExp("\\b(" + expStr + ")\\b", "gi"), " ")
+      .replace(/\s{2,}/g, " ");
+  }
+};
+
+const Restaurant = ({ restaurant }) => {
   return (
-    <div className='restaurant-card'>
-      <Link to={`/restaurants/${restaurant._id}`} className='restaurant-link'>
-      <h3 className='restaurant-name'> {restaurant.name} </h3>
-      <h4 className='restaurant-description'> {restaurant.description} </h4>
-      <h5 className='restaurant-address'> {restaurant.location.street}  {restaurant.location.streetNumber} {restaurant.location.city} </h5>
+    <div className="restaurant-card">
+      <Link to={`/restaurants/${restaurant._id}`} className="restaurant-link">
+        <h3 className="restaurant-name"> {restaurant.name} </h3>
+        <h4 className="restaurant-description"> {restaurant.description} </h4>
+        <h5 className="restaurant-address">
+          {" "}
+          {restaurant.location.street} {restaurant.location.streetNumber}{" "}
+          {removeWord(restaurant.location.city)}{" "}
+        </h5>
       </Link>
     </div>
   );
